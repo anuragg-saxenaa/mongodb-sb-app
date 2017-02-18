8f19c1102ba0b24fc9f2a77c4da12a9c129eea72
anura <anurag.saxyena@gmail.com>
2017-02-18 15:16:06 -0500

    Initial commit

---
+++ src/main/java/com/mycode/anurag/MongodbSbAppApplication.java
@@ -0,0 +1,12 @@
+package com.mycode.anurag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbSbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbSbAppApplication.class, args);
+	}
+}
